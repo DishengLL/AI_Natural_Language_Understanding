77fcb42b8150a76172cfbffec6688fa90640538c
LDS <39432361+Ldscrawler2018@users.noreply.github.com>
2022-04-07 12:02:24 -0400

    initial commit

--- code_section/ExtractOpinions.py
+++ code_section/ExtractOpinions.py
@@ -1,5 +1,7 @@
-import StringDouble
-import ExtractGraph
+# import StringDouble
+# import ExtractGraph
+import stanford_parse as NLP
+import re
 
 
 class ExtractOpinions:
@@ -9,8 +11,50 @@ class ExtractOpinions:
     extracted_opinions = {}
 
     def __init__(self):
+
         return
 
+    # preprocessing text before parsing
+    def preprocessing(self,review_content):
+        plain_text = review_content
+        plain_text = plain_text.strip()
+        plain_text = plain_text.replace('  ',' ')
+        ## try whether or not strip punctuation.
+        #plain_text = re.sub(r'[^\w\s]', '', plain_text)
+        # plain_text = plain_text.lower()
+
+
+
+        return plain_text
+
     def extract_pairs(self, review_id, review_content):
+        sNLP = NLP.StanfordNLP()
+
+        text =self.preprocessing(review_content)
+        # print(text)
+
+        a = sNLP.dependency_parse(text)
+        text = text.split(' ')
+        for i, j, k in a:
+            if i == 'ROOT':
+                continue
+                print('>>>>', text[k - 1], i)
+            l = ['nsubj', 'amod']
+            if i in l:
+                # print('\t>>>>', text[j - 1], text[k - 1], '-->', i)
+                review_content = text[j - 1]+',  '+text[k - 1]
+                # print(j-1,k-1)
+                # print(review_content, i)
+            # print(text[j - 1], text[k - 1], '-->', i)
         # example data, which you will need to remove in your real code. Only for demo.
-        self.extracted_opinions = {'service, good': [1, 2, 5], 'service, excellent': [4, 6]}
+                tem = self.extracted_opinions.get(review_content,0)
+                if tem ==0:
+                    self.extracted_opinions[review_content] = [review_id]
+                else:
+                    if review_id in tem:
+                        continue
+                    else:
+                        self.extracted_opinions[review_content].append(review_id)
+
+
+        # self.extracted_opinions = {'service, good': [1, 2, 5], 'service, excellent': [4, 6]}
