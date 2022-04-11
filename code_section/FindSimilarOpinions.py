065ebcf6cffbcf0351e6e74cf865fa443e861e96
LDS <39432361+Ldscrawler2018@users.noreply.github.com>
2022-04-11 12:33:36 -0400

    find similar opinion

--- code_section/FindSimilarOpinions.py
+++ code_section/FindSimilarOpinions.py
@@ -6,19 +6,104 @@ class FindSimilarOpinions:
     word2VecObject = []
     cosine_sim = 0
 
+    # input_extracted_ops --- a dicttionary containing {review text: [docId]}
     def __init__(self, input_cosine_sim, input_extracted_ops):
         self.cosine_sim = input_cosine_sim
         self.extracted_opinions = input_extracted_ops
         word2vec_add = "data//assign4_word2vec_for_python.bin"
         self.word2VecObject = word2vec.KeyedVectors.load_word2vec_format(word2vec_add, binary=True)
+        token_corpus = {}
+        token = set()
+
+        # token_corpus dict ---- {token: [doc_ID]}
+        for i, j in input_extracted_ops.items():
+            a, b = i.split(', ')
+            token_corpus[a] = token_corpus.get(a, []) + j
+            token_corpus[b] = token_corpus.get(b, []) + j
+        self.token_corpus = token_corpus
+        #         print('the number of token in the corpus:',len(self.token_corpus ))
         return
 
     def get_word_sim(self, word_1, word_2):
         return self.word2VecObject.similarity(word_1, word_2)
 
+    def similar_or_not(self, n, opinion, similar_,id):
+        # get similar token in the whole dataset
+        count=0
+        try:
+            example_similarity = self.get_word_sim(opinion, n)
+            if example_similarity >= self.cosine_sim:
+                similar_[opinion] = (example_similarity,id)
+
+        except KeyError:
+            #                 print(f'do not exist this term: **{opinion}**, the program just skip this term and continue to run following term')
+            try:
+                example_similarity = self.get_word_sim(opinion.lower(), n)
+                if example_similarity >= self.cosine_sim:
+                    similar_[opinion] = (example_similarity,id)
+
+            except:
+                #                     print(f'>>do not exist this term: **{opinion.lower()}**, the program just skip this term and continue to run following term')
+                try:
+                    if example_similarity >= self.cosine_sim:
+                        similar_[opinion] = (example_similarity,id)
+
+                except:
+                    #                         print(f'>>>>do not exist this term: **{ps.stem(opinion.lower())}**, the program just skip this term and continue to run following term\n')
+                    count += 1
+        return similar_
+
     def findSimilarOpinions(self, query_opinion):
         # example data, which you will need to remove in your real code. Only for demo.
-        example_similarity = self.get_word_sim("great", "good")
-        print("Similarity of 'great' and 'good' is " + str(example_similarity))
-        similar_opinions = {'service, good': [1, 2, 3], 'service, excellent': [11, 12]}
+        # example_similarity = self.get_word_sim("great", "good")
+        # print("Similarity of 'great' and 'good' is " + str(example_similarity))
+        # similar_opinions = {'service, good': [1, 2, 3], 'service, excellent': [11, 12]}
+        # query_opinion ---- input opinion
+        similar_nn = {}  # store similar token--nn
+        similar_adj = {}  # store similar token--adj
+        similar_opinions = {}  # get final similar review
+        n, adj = query_opinion.split(', ')  # get NN and adj in the query review
+        count = 0  # count the number of missing token in small word2vec database
+
+        # get similar token in the whole dataset
+        for opinion,id in self.token_corpus.items():
+            similar_nn = self.similar_or_not(n, opinion, similar_nn,id)
+        for opinion,id in self.token_corpus.items():
+            similar_adj = self.similar_or_not(adj, opinion, similar_adj,id)
+        print(similar_nn)
+        print(similar_adj)
+        similar_nn_l = []
+        similar_adj_l = []
+
+        for _,j in similar_nn.items():
+            similar_nn_l.extend(j[-1])
+        for _, j in similar_adj.items():
+            similar_adj_l.extend(j[-1])
+        print(similar_nn_l)
+        print(similar_adj_l)
+        intersecion = set(similar_adj_l).intersection(set(similar_nn_l))
+        print(intersecion)
+
+
+
+
+
         return similar_opinions
+
+if __name__ == "__main__":
+    cosine_sim = 0.2
+
+    extracted_opinions={'visited,  I': [1], 'co-worker,  old': [1], 'meal,  wonderful': [1], 'had,  We': [1], 'huge,  which': [1], 'came,  It': [1], 'stick,  little': [1], 'delicious,  which': [1], 'excellent,  Service': [1, 2], 'place,  This': [1], 'place,  great': [1], 'experience,  upscale': [1], 'spots,  other': [1], 'looking,  I': [1], 'restaurant,  old': [2], 'woodwork,  ornate': [2], 'tablecloths,  white': [2], 'had,  I': [2, 5, 19], 'potatoes,  red': [2], 'potatoes,  skinned': [2], 'potatoes,  mashed': [2], 'tender,  meat': [2], 'delicious,  slaw': [2], 'picky,  I': [2], 'meant,  We': [3], 'did,  we': [3], 'nice,  atmosphere': [3], 'list,  local': [3], 'great,  list': [3], 'world,  taco': [3], 'potions,  HUGE': [3], 'shared,  We': [3], 'huge,  menu': [3], 'thing,  that': [3], 'thing,  good': [3], 'regulars,  people': [3], 'return,  We': [3], 'start,  I': [4], 'note,  positive': [4], 'delicious,  meal': [4], 'recommend,  I': [4, 12, 15], 'sandwich,  grilled': [4], 'amazing,  fries': [4], 'option,  choose': [4], 'something,  option': [4], 'love,  I': [4], 'has,  atmosphere': [4], 'ambiance,  nice': [4], 'seemed,  they': [4], 'selection,  great': [4], 'rude,  server': [4], 'dropped,  she': [4], 'short,  She': [4], 'are,  meals': [4], 'wanted,  We': [4], 'jumped,  she': [4], 'assume,  I': [4], 'said,  she': [4], 'young,  we': [4], 'ordered,  We': [4], 'note,  thing': [4], 'small,  restaurant': [4], 'close,  tables': [4], 'asked,  hostess': [4], 'table,  OWN': [4], 'put,  we': [4], 'ride,  useless': [5], 'distance,  walking': [5], 'one,  it': [5], 'meals,  best': [5], 'fan,  I': [5], 'fan,  huge': [5], 'great,  Service': [5], 'large,  drinks': [5], 'ordered,  Girlfriend': [5], 'sandwiches,  French': [5], 'says,  she': [5], 'restaurant,  nice': [5], 'restaurant,  small': [5], 'atmosphere,  great': [5], 'food,  great': [5], 'rotation,  place': [6], 'rotation,  regular': [6], 'food,  Excellent': [6], 'awesome,  Devonshire': [6], 'like,  I': [6], 'deserts,  recent': [6], 'deserts,  mini': [6], 'think,  I': [6], 'deal,  bottle': [6], 'deal,  great': [6], 'good,  selection': [6], 'seem,  mugs': [6], 'order,  we': [7], 'undercooked,  They': [7], 'bad,  quality': [7], 'care,  They': [7], 'is,  humbleness': [7], 'day,  It': [7], 'day,  bad': [7], 'went,  I': [7], 'food,  Good': [8], 'food,  hearty': [8], 'prices,  decent': [8], 'service,  warm': [8], 'fish,  battered': [8], 'treat,  trout': [8], 'good,  bad': [8], 'service,  Bad': [9], 'ask,  We': [9], 'fingers,  homemade': [10], 'good,  Burgers': [10], 'beer,  Good': [10], 'beer,  priced': [10], 'value,  great': [10], 'large,  menu': [11], 'larger,  portions': [11], 'reasonable,  prices': [11], 'excellent,  quality': [11], 'have,  They': [11, 20], 'menu,  entire': [11], 'dedicated,  menu': [11], 'specialities,  Other': [11], 'cakes,  specialities': [11], 'come,  wait': [11], 'favorites,  restaurant': [12], 'favorites,  top': [12], 'favorites,  local': [12], 'has,  restaurant': [12], 'feeling,  warm': [12], 'offer,  They': [12], 'deal,  which': [12], 'deal,  cheapest': [12], 'find,  you': [12], 'generous,  portions': [12], 'order,  particular': [12], 'hungry,  you': [12], 'course,  main': [12], 'soup,  French': [12], 'dined,  I': [13], 'Summer,  last': [13], 'had,  we': [13], 'time,  wonderful': [13], 'excellent,  food': [13], 'loved,  We': [13], 'ordered,  we': [13], 'solid,  service': [13], 'group,  larger': [13], 'going,  I': [13], 'Grill,  Best': [14], 'made,  Grill': [14], 'turkey,  roasted': [14], 'service,  great': [14], 'food,  fresh': [14], 'food,  interesting': [14], 'got,  I': [15, 19], 'good,  which': [15], 'big,  it': [15], 'giving,  I': [15], 'waiter,  slow': [15], 'took,  He': [15], 'time,  long': [15], 'kept,  I': [15], 'thing,  second': [15], 'sure,  thing': [15], 'sure,  I': [15], 'forgot,  he': [15], 'took,  it': [15], 'cold,  food': [15], 'group,  Drew': [16], 'group,  large': [16], 'satisfying,  food': [16], 'food,  typical': [16], 'food,  American': [16], 'way,  bad': [16], 'WONDERFUL,  it': [16], 'options,  Vegan': [16], 'are,  options': [16], 'take,  you': [16], 'salad,  Large': [17], 'lot,  salad': [17], 'Salad,  Greek': [17], 'nachos,  Good': [17], 'fries,  great': [17], 'friendly,  waiter': [17], 'be,  We': [17], 'heard,  you': [18], 'wines,  They': [18], 'wines,  decent': [18], 'favorite,  Noir': [18], 'food,  Fast': [18], 'get,  I': [18], 'gotten,  I': [18], 'restaurants,  busy': [19], 'hopes,  high': [19], 'disappointed,  I': [19], 'suspect,  I': [19], 'ordered,  I': [19], 'OK,  food': [19], 'good,  service': [20], 'pleasant,  ambience': [20], 'see,  I': [20], 'Pittsburghers,  older': [20], 'airing,  that': [20], 'looks,  It': [20], 'is,  it': [20]}
+
+    step_2_find_similar_opinion = FindSimilarOpinions(cosine_sim, extracted_opinions)
+    opinions = ["service, good", "service, bad", "atmosphere, good", "food, delicious"]
+    for query_opinion in opinions:
+        print("\nquery opinion [" + query_opinion + "] has similar opinions: ")
+        similar_opinions = step_2_find_similar_opinion.findSimilarOpinions(query_opinion)
+
+        # print similar result
+        for tmp_opinion in similar_opinions:
+            review_ids = similar_opinions[tmp_opinion]
+            print("\n\t[" + tmp_opinion + "] appears in review " + "\t" + " ".join(str(review_ids)))
+    print("\n--------------------------------------------------------------")
+
